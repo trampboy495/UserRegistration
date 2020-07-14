517e8bb1494227e4940bf1933cb6f3590b0adfbf
trampboy <histroylover@gmail.com>
2020-07-14 13:25:18 +0530

    [siddhesh] add.UC3 validate email

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
-read -p "enter the first name and last name to validate" Firstname Lastname
-pattern='^[A-Z][A-Za-z]{2,}'
-[[ $Firstname =~ $pattern ]] && { echo "valid first name";} || { echo "invalid first name";}
-[[ $Lastname =~ $pattern ]] && { echo "valid last name";} || { echo "invalid last name";}
+#read -p "enter the first name and last name to validate" Firstname Lastname
+patternName='^[A-Z][A-Za-z]{2,}'
+patternEmail='^[A-Za-z0-9]+([.][A-Za-z0-9]+)*@[A-Za-z0-9]+\.[a-z]{2,}([.][A-Za-z0-9]+)*$'
+#[[ $Firstname =~ $patternName ]] && { echo "valid first name";} || { echo "invalid first name";}
+#[[ $Lastname =~ $patternName ]] && { echo "valid last name";} || { echo "invalid last name";}
+read -p "enter the email to validate" email
+[[ $email =~ $patternEmail ]] && { echo "valid email";} || { echo "invalid email";}
