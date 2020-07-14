e06e809bfa43a4a1badbcc8af38254164c538a74
trampboy <histroylover@gmail.com>
2020-07-14 12:33:26 +0530

    [siddhesh] add.UC2 last name to validate

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,5 @@
 #!/bin/bash -x
+read -p "enter the first name and last name to validate" Firstname Lastname
+pattern='^[A-Z][A-Za-z]{2,}'
+[[ $Firstname =~ $pattern ]] && { echo "valid first name";} || { echo "invalid first name";}
+[[ $Lastname =~ $pattern ]] && { echo "valid last name";} || { echo "invalid last name";}
