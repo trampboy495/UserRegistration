f33bb5bf841aa2a07e1bbb66cf0348b1ac7650b2
trampboy <histroylover@gmail.com>
2020-07-14 16:52:05 +0530

    [siddhesh] add.UC8 validate password rule 4

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,13 +2,13 @@
 patternName='^[A-Z][A-Za-z]{2,}'
 patternEmail='^[A-Za-z0-9]+([.+-][A-Za-z0-9]{3,})*@[A-Za-z0-9]+\.[a-z]{2,}([.][A-Za-z0-9]{2,})*$'
 patternNo='^[0-9]{2}\s[0-9]{10}$'
-patternPass='[A-Z]+[a-zA-Z0-9]{6,}[0-9]+'
-read -p "enter the first name and last name to validate" Firstname Lastname
-[[ $Firstname =~ $patternName ]] && { echo "valid first name";} || { echo "invalid first name";}
-[[ $Lastname =~ $patternName ]] && { echo "valid last name";} || { echo "invalid last name";}
-read -p "enter the email to validate" email
-[[ $email =~ $patternEmail ]] && { echo "valid email";} || { echo "invalid email";}
-read -p "enter the phone no to validate" PhoneNo
-[[ $PhoneNo =~ $patternNo ]] && { echo "valid phone no";} || { echo "invalid phone no";}
+patternPass='[A-Z]+[a-zA-Z0-9]{5,}[.$#@*&]{1}[0-9]+'
+#read -p "enter the first name and last name to validate" Firstname Lastname
+#[[ $Firstname =~ $patternName ]] && { echo "valid first name";} || { echo "invalid first name";}
+#[[ $Lastname =~ $patternName ]] && { echo "valid last name";} || { echo "invalid last name";}
+#read -p "enter the email to validate" email
+#[[ $email =~ $patternEmail ]] && { echo "valid email";} || { echo "invalid email";}
+#read -p "enter the phone no to validate" PhoneNo
+#[[ $PhoneNo =~ $patternNo ]] && { echo "valid phone no";} || { echo "invalid phone no";}
 read -p "enter the password to validate" passwd
 [[ $passwd =~ $patternPass ]] && { echo "valid password";} || { echo "invalid password";}
