106d63377f524b58f93766e8512e61bdcaec843c
trampboy <histroylover@gmail.com>
2020-07-15 11:54:30 +0530

    [siddhesh] refator.master uncommented commented lines

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,13 +6,13 @@ patternpass1='.{8,}'
 patternpass2='[A-Z]+'
 patternpass3='[0-9]+'
 patternpass4='^[a-zA-Z0-9]*[.$#@*&]$'
-#read -p "enter the first name and last name to validate" Firstname Lastname
-#[[ $Firstname =~ $patternName ]] && { echo "valid first name";} || { echo "invalid first name";}
-#[[ $Lastname =~ $patternName ]] && { echo "valid last name";} || { echo "invalid last name";}
-#read -p "enter the email to validate" email
-#[[ $email =~ $patternEmail ]] && { echo "valid email";} || { echo "invalid email";}
-#read -p "enter the phone no to validate" PhoneNo
-#[[ $PhoneNo =~ $patternNo ]] && { echo "valid phone no";} || { echo "invalid phone no";}
+read -p "enter the first name and last name to validate" Firstname Lastname
+[[ $Firstname =~ $patternName ]] && { echo "valid first name";} || { echo "invalid first name";}
+[[ $Lastname =~ $patternName ]] && { echo "valid last name";} || { echo "invalid last name";}
+read -p "enter the email to validate" email
+[[ $email =~ $patternEmail ]] && { echo "valid email";} || { echo "invalid email";}
+read -p "enter the phone no to validate" PhoneNo
+[[ $PhoneNo =~ $patternNo ]] && { echo "valid phone no";} || { echo "invalid phone no";}
 read -p "enter the password to validate" passwd
 [[ $passwd =~ $patternpass1 && $passwd =~ $patternpass2 && $passwd =~ $patternpass3 && $passwd =~ $patternpass4 ]] && \
 { echo "valid password";} || { echo "invalid password";}
